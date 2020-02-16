b4a8f3afe1e3b1f18ed5b4714f047897ee2d14e6
Sascha Engmann <engmann@w11k.de>
2020-02-16 13:31:34 +0100

    add missing lifecycles

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { ReplaySubject, Observable } from "rxjs";
-import { SimpleChanges } from "@angular/core";
+import { 
+    SimpleChanges, 
+    OnDestroy, 
+    OnInit, 
+    OnChanges, 
+    AfterViewInit, 
+    AfterViewChecked,
+    AfterContentChecked,
+    AfterContentInit 
+} from "@angular/core";
 
-export class OnDestroyMixin {
+export class OnDestroyMixin implements OnDestroy {
     onDestroy$ = new ReplaySubject<void>(1);
 
     observeOnDestroy(): Observable<void> {
@@ -14,7 +23,7 @@ export class OnDestroyMixin {
     }
 }
 
-export class OnInitMixin {
+export class OnInitMixin implements OnInit {
     onInit$ = new ReplaySubject<void>(1);
 
     observeOnInit(): Observable<void> {
@@ -23,10 +32,11 @@ export class OnInitMixin {
 
     ngOnInit() {
         this.onInit$.next();
+        this.onInit$.complete();
     }
 }
 
-export class OnChangesMixin {
+export class OnChangesMixin implements OnChanges {
     onChanges$ = new ReplaySubject<SimpleChanges>(1);
 
     observeOnChanges(): Observable<SimpleChanges> {
@@ -35,5 +45,57 @@ export class OnChangesMixin {
 
     ngOnChanges(changes: SimpleChanges) {
         this.onChanges$.next(changes);
+    }
+}
+
+export class OnAfterViewChecked implements AfterViewChecked {
+    onAfterViewChecked$ = new ReplaySubject<void>(1);
+
+    observeAfterViewChecked() : Observable<void> {
+        return this.onAfterViewChecked$.asObservable();
+    }
+
+    ngAfterViewChecked() {
+        this.onAfterViewChecked$.next();
+        this.onAfterViewChecked$.complete();
+    }
+}
+
+export class OnAfterViewInitMixin implements AfterViewInit {
+    onAfterViewInit$ = new ReplaySubject<void>(1);
+
+    observeOnAfterViewInit() : Observable<void> {
+        return this.onAfterViewInit$.asObservable();
+    }
+
+    ngAfterViewInit() {
+        this.onAfterViewInit$.next();
+        this.onAfterViewInit$.complete();
+    }
+}
+
+export class OnAfterContentChecked implements AfterContentChecked {
+    onAfterContentChecked$ = new ReplaySubject<void>(1);
+
+    observeOnAfterContentChecked() : Observable<void> {
+        return this.onAfterContentChecked$.asObservable();
+    }
+
+    ngAfterContentChecked() {
+        this.onAfterContentChecked$.next();
+        this.onAfterContentChecked$.complete();
+    }
+}
+
+export class OnAfterContentInit implements AfterContentInit {
+    onAfterContentInit$ = new ReplaySubject<void>(1);
+
+    observeOnAfterContentInit() : Observable<void> {
+        return this.onAfterContentInit$.asObservable();
+    }
+
+    ngAfterContentInit() {
+        this.onAfterContentInit$.next();
+        this.onAfterContentInit$.complete();
     }
 }
